9cf7ddf4938d6582224e1a4e30bf20f6f324af22
Kyle Jones <sornaensis@gmail.com>
2016-04-18 14:43:34 -0400

    Refactor + Updated structures

--- Server/Game.hs
+++ Server/Game.hs
@@ -1,59 +1,59 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric, DeriveDataTypeable, TypeFamilies, TemplateHaskell #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE TemplateHaskell    #-}
+{-# LANGUAGE TypeFamilies       #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 module Game where
 
-import Control.Monad.Reader
-import Control.Monad.State
-import Data.Acid
-import Data.Array
-import Data.Maybe
-import Data.SafeCopy
-import Data.Typeable
-import Data.Either
-import qualified Data.Map.Strict as M
-import Network.Socket
-import GHC.Generics
-import Data.Aeson (fromJSON, toJSON, FromJSON(..), ToJSON(..))
-import qualified Data.ByteString.Lazy.Char8 as C
-import System.IO
-import System.Random
-import System.IO.Error
+import           Control.Monad.Reader
+import           Control.Monad.State
+import           Data.Acid
+import           Data.Aeson           (FromJSON (..), ToJSON (..))
+import           Data.Array
+import qualified Data.Map.Strict      as M
+import           Data.Maybe
+import           Data.SafeCopy
+import           Data.Typeable
+import           GHC.Generics
+import           System.Random
 
-data Action     = Play | Rest | DrawCard | TapDown deriving (Show, Generic, Eq)
-instance FromJSON Action 
+data Action     = Play | Rest | Stand | TapDown deriving (Show, Generic, Eq)
+instance FromJSON Action
 instance ToJSON Action
 
 data GameAction = GameAction { action :: Action
                              , card   :: Maybe Int } deriving (Show, Generic)
-instance FromJSON GameAction 
+instance FromJSON GameAction
 instance ToJSON GameAction
 
-data CardEffect = Pin 
-                | Submit 
+data CardEffect = Pin
+                | Submit
                 | Skip
                 | Draw Int
                 | KnockDown
-                | Counter 
-                | Escape 
+                | Counter
+                | Escape
                 | Attack Int
                 | MustBeDown
                 | StandUp
                 | Special deriving (Show, Eq, Generic)
-instance FromJSON CardEffect 
+instance FromJSON CardEffect
 instance ToJSON CardEffect
 
 data Stance   = Up | Kneel | Down deriving (Eq, Show, Generic)
-instance FromJSON Stance 
+instance FromJSON Stance
 instance ToJSON Stance
 
 data Game     = Running | P1Win | P2Win deriving (Show, Eq, Generic)
 instance FromJSON Game
 instance ToJSON Game
 
-data Character = Giant | Luchador | Brawler | Technician | Hero deriving (Show, Generic, Eq) 
+data Character = Giant | Luchador | Brawler | Technician | Hero deriving (Show, Generic, Eq)
 instance FromJSON Character
 instance ToJSON Character
 
-data Player = Player { 
+data Player = Player {
                 name,pw     :: String
               , wins,losses :: Int } deriving (Show, Eq, Generic, Typeable)
 instance FromJSON Player
@@ -65,20 +65,21 @@ data GamePlayer = GamePlayer {
               , deck      :: [Int]
               , character :: Character
               , health    :: Int
+              , tapdown   :: Bool
               , stance    :: Stance } deriving (Show, Eq, Generic)
 instance FromJSON GamePlayer
 instance ToJSON GamePlayer
 
 data GameState = GameState {
-                p1, p2  :: GamePlayer
-              , stack   :: [Int]
-              , gstate  :: Game
-              , p1turn  :: Bool } deriving (Show, Eq, Generic)
+                p1, p2 :: GamePlayer
+              , stack  :: [Int]
+              , gstate :: Game
+              , p1turn :: Bool } deriving (Show, Eq, Generic)
 instance FromJSON GameState
 instance ToJSON GameState
-                    
+
 data Card = Card {
-                  getEffects   :: [CardEffect]
+                  getEffects :: [CardEffect]
                  } deriving (Show, Eq, Generic)
 instance FromJSON Card
 instance ToJSON Card
@@ -86,10 +87,10 @@ instance ToJSON Card
 type Cards = Array Int Card
 
 theCards :: Cards
-theCards = listArray (0,106) 
-            $! fmap Card 
+theCards = listArray (0,106)
+            $! fmap Card
             [
-                [Attack 5, KnockDown]
+                [Attack 5, Special, KnockDown]
             ,   [Attack 2, KnockDown]
             ,   [Counter, Submit]
             ,   [Attack 1]
@@ -127,7 +128,7 @@ theCards = listArray (0,106)
             ,   [Attack 2]
             ,   [Attack 1, Skip]
             ,   [Attack 2, Pin, KnockDown]
-            ,   [Attack 5, KnockDown]
+            ,   [Attack 5, Special, KnockDown]
             ,   [Submit]
             ,   [Attack 2, Counter]
             ,   [Attack 2]
@@ -135,7 +136,7 @@ theCards = listArray (0,106)
             ,   [Attack 2]
             ,   [Attack 1, Counter]
             ,   [Attack 1, KnockDown]
-            ,   [Attack 5, KnockDown]
+            ,   [Attack 5, Special, KnockDown]
             ,   [Attack 2, Pin, KnockDown]
             ,   [Counter, Draw 1]
             ,   [Counter, StandUp]
@@ -154,7 +155,7 @@ theCards = listArray (0,106)
             ,   [Counter, StandUp]
             ,   [Attack 3, KnockDown]
             ,   [Pin, MustBeDown]
-            ,   [Attack 5, MustBeDown]
+            ,   [Attack 5, Special, MustBeDown]
             ,   [Attack 3, Counter, KnockDown]
             ,   [Attack 1]
             ,   [Attack 0, KnockDown]
@@ -166,7 +167,7 @@ theCards = listArray (0,106)
             ,   [Attack 4, MustBeDown]
             ,   [Attack 3, MustBeDown]
             ,   [Pin]
-            ,   [Submit]
+            ,   [Submit, Special]
             ,   [Submit]
             ,   [Pin, MustBeDown]
             ,   [Attack 2]
@@ -196,37 +197,35 @@ theCards = listArray (0,106)
             ,   [Pin]
             ,   [Counter, Draw 2]
             ,   [Attack 2, MustBeDown]
-                
+
             ]
 mkGameState :: StdGen -> GamePlayer -> GamePlayer -> GameState
 mkGameState g p1 p2 = GameState p1 p2 [] Running (fst $ randomR (True, False) g)
 
 mkGamePlayer :: StdGen -> Player -> Character -> GamePlayer
-mkGamePlayer g p c = let (hand, deck) = splitAt 5 . take 70 $ (randomRs (0,106) g :: [Int])
-                     in GamePlayer p hand deck c 25 Up
+mkGamePlayer g p c = let (hand, deck) = splitAt 4 . take 50 $ (randomRs (0,106) g :: [Int])
+                     in GamePlayer p hand deck c 25 False Up
 
 nextTurn :: GameState -> GameState
-nextTurn (GameState p1 p2 s r t) = GameState p1 p2 s r (not t)
-
--- resolveStack :: GameState -> 
+nextTurn g = g { p1turn = not $ p1turn g }
 
 -- Initial Connection
 data ConnPlayer = ConnPlayer { username,pass :: String, queued :: Bool, chara :: Maybe Character } deriving (Show, Generic, Eq)
-instance FromJSON ConnPlayer 
+instance FromJSON ConnPlayer
 instance ToJSON ConnPlayer
 
 newtype UserDB = UserDB { users :: M.Map String Player } deriving (Typeable)
 
 validatePlayer :: ConnPlayer -> Query UserDB (Maybe Player)
-validatePlayer (ConnPlayer n p _ _) = asks $ (\db -> 
+validatePlayer (ConnPlayer n p _ _) = asks $ (\db ->
                                             case M.lookup n db of
                                                Nothing -> Nothing
-                                               Just u  -> if pw u == p 
+                                               Just u  -> if pw u == p
                                                            then Just u
-                                                           else Nothing) . users 
+                                                           else Nothing) . users
 
 exists :: ConnPlayer -> Query UserDB Bool
-exists (ConnPlayer n _ _ _) = asks $ isNothing . (\db -> M.lookup n db) . users
+exists (ConnPlayer n _ _ _) = asks $ isNothing . M.lookup n . users
 
 newPlayer :: ConnPlayer -> Update UserDB ()
 newPlayer (ConnPlayer n p _ _) = modify go
--- Server/Server.hs
+++ Server/Server.hs
@@ -1,40 +1,41 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 module Server where
 
-import Game
+import           Game
 
-import Data.Acid
-import Data.Array
-import Data.Maybe
-import Data.SafeCopy
-import Data.Typeable
-import qualified Data.Map.Strict as M
-import Control.Concurrent
-import Control.Concurrent.MVar
-import Control.Concurrent.Chan
-import Control.Monad
-import Control.Applicative
-import Control.Monad.State
-import Control.Monad.Reader
-import Control.Monad.Fix
-import Network.Socket
-import GHC.Generics
-import Data.Aeson
-import Data.Aeson.Types
+import           Control.Applicative
+import           Control.Concurrent
+import           Control.Concurrent.Chan
+import           Control.Concurrent.MVar
+import           Control.Monad
+import           Control.Monad.Fix
+import           Control.Monad.Reader
+import           Control.Monad.State
+import           Data.Acid
+import           Data.Aeson
+import           Data.Aeson.Types
+import           Data.Array
 import qualified Data.ByteString.Lazy.Char8 as C
-import System.IO
-import System.IO.Error
-import System.Random
+import qualified Data.Map.Strict            as M
+import           Data.Maybe
+import           Data.SafeCopy
+import           Data.Typeable
+import           GHC.Generics
+import           Network.Socket
+import           System.IO
+import           System.IO.Error
+import           System.Random
 
 -- Connections manage game players and their socket handles
 type Connection = (ConnPlayer, Handle)
 
 main :: IO ()
 main =  do
-         sock <- socket AF_INET Stream 0    
-         setSocketOption sock ReuseAddr 1   
-         bind sock (SockAddrInet 13337 iNADDR_ANY)   
-         listen sock 2                              
+         sock <- socket AF_INET Stream 0
+         setSocketOption sock ReuseAddr 1
+         bind sock (SockAddrInet 13337 iNADDR_ANY)
+         listen sock 2
          comm <- newEmptyMVar
          db <- openLocalState (UserDB M.empty)
          catchIOError (do forkIO $ playQueue [] comm db
@@ -44,7 +45,7 @@ main =  do
 
 snd' (_, a, _) = a
 
-pingConn hdl = fix $ \loop -> do 
+pingConn hdl = fix $ \loop -> do
                             res <- tryIOError (hPutStrLn hdl "PING" >> threadDelay 400000)
                             case res of
                                 Left _ -> hClose hdl
@@ -60,45 +61,41 @@ playQueue cs mv db = do (c, hdl) <- takeMVar mv
                         ps <- filterM (hIsOpen . snd' . fst) cs
                         print ps
                         case p of
-                           Nothing  -> hPutStrLn hdl "{\"error\":\"Invalid Password\"}" 
+                           Nothing  -> hPutStrLn hdl "{\"error\":\"Invalid Password\"}"
                                        >> hClose hdl >> playQueue ps mv db
-                           (Just p) -> 
-                                 do sendStats (p, hdl, chr)
+                           (Just p) ->
+                                 do sendStats (p, hdl, chr) >> threadDelay 40000
                                     if queued c && isJust chr
-                                       then if not . null $ ps 
-                                             then do forkIO $ killThread (snd $ last ps) 
+                                       then if not . null $ ps
+                                             then do forkIO $ killThread (snd $ last ps)
                                                                 >> makeMatch (p, hdl, chr) (fst $ last ps) db
                                                      playQueue (init ps) mv db
                                              else forkIO (pingConn hdl) >>= \t -> playQueue (((p,hdl,chr),t):ps) mv db
-                                       else playQueue ps mv db
+                                       else forkIO (threadDelay 40000 >> hClose hdl) >> playQueue ps mv db
 
 type VConnection = (Player, Handle, Maybe Character)
 
 sendStats :: VConnection -> IO ()
-sendStats (Player n _ w l, hdl, _) = hPutStrLn hdl $ C.unpack (encode $ Player n "" w l) 
+sendStats (Player n _ w l, hdl, _) = hPutStrLn hdl (C.unpack (encode $ Player n "" w l))
 
 makeMatch :: VConnection -> VConnection -> AcidState UserDB -> IO ()
-makeMatch (p1, h1, Just c1) (p2, h2, Just c2) db =
-         do g1 <- newStdGen
-            g2 <- newStdGen
-            g3 <- newStdGen
-            let gp1  = mkGamePlayer g1 p1 c1
-                gp2  = mkGamePlayer g2 p2 c2
+makeMatch (p1', h1, Just c1) (p2', h2, Just c2) db =
+         do [g1, g2, g3] <- replicateM 3 newStdGen
+            let gp1  = mkGamePlayer g1 p1' c1
+                gp2  = mkGamePlayer g2 p2' c2
                 game = mkGameState  g3 gp1 gp2
             --- |     ALL GAME LOGIC HAPPENS HERE     | ---
-            p1in  <- newEmptyMVar
-            p2in  <- newEmptyMVar
+            [p1in, p2in, comm] <- replicateM 3 newEmptyMVar
             error <- newEmptyMVar
-            comm  <- newEmptyMVar
-            thr1 <- forkIO $ fix $ \loop -> do res <- tryIOError ((C.pack . init) <$> (hGetLine h1) >>= tryPutMVar p1in)
+            thr1 <- forkIO $ fix $ \loop -> do res <- tryIOError (C.pack . init <$> hGetLine h1 >>= tryPutMVar p1in)
                                                case res of
                                                 Left _  -> putMVar error True
                                                 Right _ -> loop
-            thr2 <- forkIO $ fix $ \loop -> do res <- tryIOError ((C.pack . init) <$> (hGetLine h2) >>= tryPutMVar p2in)
+            thr2 <- forkIO $ fix $ \loop -> do res <- tryIOError (C.pack . init <$> hGetLine h2 >>= tryPutMVar p2in)
                                                case res of
                                                 Left _  -> putMVar error True
                                                 Right _ -> loop
-            gthrd <- forkIO $ fix (\loop g -> 
+            gthrd <- forkIO $ fix (\loop g ->
                    case gstate g of
                      Running -> do synchGame h1 h2 g
                                    putStrLn $! if p1turn g then "Player 1 Turn" else "Player 2 Turn"
@@ -107,32 +104,35 @@ makeMatch (p1, h1, Just c1) (p2, h2, Just c2) db =
                                    tryTakeMVar turn
                                    tryTakeMVar response
                                    read <- forkIO $ tryTakeMVar response >> takeMVar turn >>= putMVar comm
-                                   ctrl <- forkIO $ threadDelay 10000000 >> killThread read 
-                                                        >> putMVar comm restAct 
-                                   act <- decode' <$> takeMVar comm 
+                                   ctrl <- forkIO $ threadDelay 10000000 >> killThread read
+                                                        >> putMVar comm restAct
+                                   act <- takeMVar comm
+                                   print act
                                    killThread ctrl
                                    putStrLn "End of turn"
-                                   case act of 
+                                   case decode' act of
                                     Nothing -> putMVar error True
-                                    Just var -> 
+                                    Just var ->
                                           case action var of
-                                           Play     -> case card var of 
+                                           Play     -> case card var of
                                                         Nothing -> putMVar error True
                                                         Just c  -> if validateCard c g
                                                                     then
-                                                                     do g' <- fix (\loop st -> 
+                                                                     do g' <- fix (\loop st tn resp ->
                                                                                 do synchGame h1 h2 st
                                                                                    return st
-                                                                               ) (startStack g c)
+                                                                               ) (startStack g c) turn response
                                                                         loop $! nextTurn . resolveStack $ g'
                                                                     else
-                                                                     loop $! nextTurn g
-                                           DrawCard -> loop $! nextTurn (drawCard g)
-                                           Rest     -> loop $! nextTurn (restPlayer g) 
-                                           _        -> putMVar error True 
-                     P1Win       -> putMVar error False 
-                     P2Win       -> putMVar error False ) game 
-                     
+                                                                     loop $! drawCard $ nextTurn g
+                                           Rest     -> loop $! drawCard . nextTurn $ restPlayer g
+                                           Stand    -> loop $! drawCard . nextTurn $ if p1turn g
+                                                                                        then g { p1 = standup $ p1 g }
+                                                                                        else g { p2 = standup $ p2 g }
+                                           _        -> putMVar error True
+                     P1Win       -> putMVar error False
+                     P2Win       -> putMVar error False ) game
+
             end <- takeMVar error
             putStrLn "Game Ended"
             mapM_ killThread [gthrd, thr1, thr2]
@@ -150,17 +150,17 @@ handleBrokenPipe h1 h2 =
 
 validateCard c (GameState p1 p2 s g t) = let eff   = getEffects $ theCards ! c
                                              stand = stance $ if t then p1 else p2
-                                             inhand = c `elem` hand (if t then p1 else p2) 
-                                         in inhand && (Counter `notElem` eff) 
+                                             inhand = c `elem` hand (if t then p1 else p2)
+                                         in inhand && (Counter `notElem` eff)
                                                 && ((MustBeDown `notElem` eff) || stand == Down)
 
-restAct = encode $ GameAction Rest Nothing 
+restAct = encode $ GameAction Rest Nothing
 
 dropFirst a [] = []
 dropFirst a (x:xs) | x == a    = xs
                    | otherwise = x : dropFirst a xs
 
-startStack g i = let t = p1turn g 
+startStack g i = let t = p1turn g
                  in g { p1 = if t then (p1 g) { hand = dropFirst i . hand $ p1 g } else p1 g
                       , p2 = if t then p2 g else (p2 g) { hand = dropFirst i . hand $ p2 g }
                       , stack = [i] }
@@ -171,14 +171,14 @@ doEffects won id (GameState p1 p2 s g t) = let (plyr,opp) = if t then (p1,p2) el
                                                p1'    = if (t && won) || (not t && not won) then self else target
                                                p2'    = if (t && won) || (not t && not won) then target else self
                                                effects = getEffects $ theCards ! id
-                                           in undefined
+                                           in GameState p1' p2' s g (if Skip `elem` effects then not t else t)
                                          where
                                          runsEffect e p = case e of
                                                           Draw   n  -> if n > 0 then foldl (\p _ -> draw p) p [1..n] else p
                                                           StandUp   -> standup p
                                                           _         -> p
                                          runtEffect e p = case e of
-                                                          Attack n  -> p { health = health p - n }       
+                                                          Attack n  -> p { health = health p - n }
                                                           Draw   n  -> if n < 0 then p { hand = drop (abs n) $ hand p } else p
                                                           KnockDown -> knock p
                                                           _         -> p
@@ -194,7 +194,7 @@ knock p | stance p == Up && character p /= Giant  = p { stance = Down }
         | otherwise                                  = p
 
 resolveStack (GameState p1 p2 s g t) = let game = GameState p1 p2 [] g t
-                                           win = odd . length $ s 
+                                           win = odd . length $ s
                                        in foldr (doEffects win) game $ processStack s
                                      where
                                      processStack [l]      = [l]
@@ -203,10 +203,10 @@ resolveStack (GameState p1 p2 s g t) = let game = GameState p1 p2 [] g t
 drawCard (GameState p1 p2 s g t) | t         = GameState (draw p1) p2 s (canDraw p1 P2Win) t
                                  | otherwise = GameState p1 (draw p2) s (canDraw p2 P1Win) t
 
-canDraw (GamePlayer _ _ (_:_) _ _ _) _ = Running
-canDraw (GamePlayer _ _ _     _ _ _) s = s
-draw (GamePlayer p h (d:ds) c he s) = GamePlayer p (drop (length h-4) $ h++[d]) ds c he s
-draw (GamePlayer p h ds     c he s) = GamePlayer p  h       ds c he s
+canDraw (GamePlayer _ _ (_:_) _ _ _ _) _ = Running
+canDraw GamePlayer {}                s = s
+draw (GamePlayer p h (d:ds) c he t s) = GamePlayer p (drop (length h-4) $ h++[d]) ds c he t s
+draw (GamePlayer p h ds     c he t s) = GamePlayer p  h       ds c he t s
 
 restPlayer (GameState p1 p2 s g t) | t         = GameState (incHealth p1) p2 s g t
                                    | otherwise = GameState p1 (incHealth p2) s g t
@@ -218,22 +218,23 @@ synchGame h1 h2 (GameState p1 p2 s g t) = do let p1' = extract p1
                                                  p2' = extract p2
                                              hPutStrLn h1 . C.unpack . encode $ GameState p1  p2' s g t
                                              hPutStrLn h2 . C.unpack . encode $ GameState p1' p2  s g t
-                                        where 
-                                        extract (GamePlayer p _ _ c h s) = GamePlayer (strip p) [] [] c h s
+                                        where
+                                        extract (GamePlayer p _ _ c h t s) = GamePlayer (strip p) [] [] c h t s
                                         strip (Player n _ w l)           = Player n "" w l
 
 mainLoop :: Socket -> MVar Connection ->  IO ()
-mainLoop sock mv = 
-            do
-             conn <- accept sock     
-             forkIO $ runConn conn mv
-             mainLoop sock mv
+mainLoop sock mv = fix $ \loop ->
+                       do
+                        conn <- accept sock
+                        forkIO $ runConn conn mv
+                        loop
 
 runConn :: (Socket, SockAddr) -> MVar Connection -> IO ()
 runConn (sock, _) mv = do
                           hdl <- socketToHandle sock ReadWriteMode
                           hSetBuffering hdl NoBuffering
-                          line <- fmap (C.pack . init) (hGetLine hdl) 
-                          case decode' line :: Maybe ConnPlayer of
-                            Just a  -> putMVar mv (a, hdl)
-                            Nothing -> hClose hdl 
+                          void . tryIOError $
+                                       do line <- C.pack . init <$> hGetLine hdl
+                                          case decode' line of
+                                            Just a  -> putMVar mv (a, hdl)
+                                            Nothing -> hClose hdl
